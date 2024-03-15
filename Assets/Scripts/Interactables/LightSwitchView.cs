d50bf13cc8f65a18711cbfbe0ea14c1f015943eb
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-03-15 05:39:39 +0530

    Delegate Implementation for the LightSwitch.

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -6,11 +6,21 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
 
+    //delegate implementation
+    public delegate void LightSwitchDelegate(); //Signature
+    public LightSwitchDelegate lightSwitch;       //Instance
+
+    private void OnEnable()
+    {
+        lightSwitch = OnLightSwitchToggled;
+    }
+
     private void Start() => currentState = SwitchState.Off;
 
     public void Interact()
     {
         //Todo - Implement Interaction
+        lightSwitch.Invoke();
     }
     private void toggleLights()
     {
@@ -33,5 +43,11 @@ public class LightSwitchView : MonoBehaviour, IInteractable
         {
             lightSource.enabled = lights;
         }
+    }
+    private void OnLightSwitchToggled()
+    {
+        toggleLights();
+        GameService.Instance.GetInstructionView().HideInstruction();
+        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
     }
 }
