e2c85d9ea99b09f4b3380085907b9f0c7ca88bbd
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-03-16 00:56:50 +0530

    Added EventController
    Created baseEvent(action) in EventController

---
+++ Assets/Scripts/Event/EventController.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class EventController 
+{
+    public Action baseEvent;
+    public void AddListener(Action listener)=> baseEvent += listener;
+    public void RemoveListener(Action listener) => baseEvent -= listener;
+    public void InvokeEvent() => baseEvent?.Invoke();
+
+
+}
