7bcc8db26d10504c98b62fd991be8e547125d590
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-03-17 17:48:09 +0530

    Implemented OnKeyPickedUp vent

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -27,11 +27,14 @@ public class PlayerController
         playerState = PlayerState.InDark;
 
         EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
+        EventService.Instance.OnkeyPickedUp.AddListener(onKeysPickedUp);
+        
     }
 
     ~PlayerController()
     {
         EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+        EventService.Instance.OnkeyPickedUp.RemoveListener(onKeysPickedUp);
     }
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
 
@@ -84,5 +87,9 @@ public class PlayerController
             PlayerState = PlayerState.None;
         else
             PlayerState = PlayerState.InDark;
+    }
+    private void onKeysPickedUp(int keys    )
+    {
+        KeysEquipped = keys;
     }
 }
