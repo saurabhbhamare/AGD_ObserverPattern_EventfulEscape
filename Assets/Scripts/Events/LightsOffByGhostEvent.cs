0d5a5288faab54d2da99dc13fcd4dfb8f21fcde4
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-03-17 23:22:56 +0530

    Camera shake and Sound added (lightsOffByGhost)

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -1,6 +1,18 @@
-using UnityEngine;
-
+using UnityEngine; 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int keysRequiredToTrigger;
+    [SerializeField] private SoundType soundType;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.GetComponent<PlayerView>()!= null && keysRequiredToTrigger == GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
+            this.enabled = false;
+
+        }
+    }
 }
